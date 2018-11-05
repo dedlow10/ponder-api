631d0044ba846280915c440db617209eb50485da
CIPHER\d.edlow <d.edlow@cipher-sys.com>
2018-11-05 16:19:17 -0500

    Code cleanup

--- functions/acceptInvitation.js
+++ functions/acceptInvitation.js
@@ -2,17 +2,19 @@ var friendsFuncs = require("../friends-funcs");
 
 module.exports = {
     handler: async function(event, context, callback) {
-        if (event["is-ping"]) context.succeed(true);
+        if (event["is-ping"]) {
+            context.succeed(true);
+            return;
+        }
         var invitedByUserId = event.context.invitedByUserId;
         var userId = event.context["authorizer-principal-id"];
         var pm = new Promise((resolve, reject) => {
-            
-        friendsFuncs.acceptInvitation(invitedByUserId, userId, new Date().toISOString(),
-            function(result) {
-                context.succeed(result);
-            },
-            function(error) {
-                callback(error);
+            friendsFuncs.acceptInvitation(invitedByUserId, userId, new Date().toISOString(),
+                function(result) {
+                    context.succeed(result);
+                },
+                function(error) {
+                    callback(error);
             });
         });
         
--- functions/addfriend.js
+++ functions/addfriend.js
@@ -2,14 +2,17 @@ var friendsFuncs = require("../friends-funcs");
 
 module.exports = {
     handler: async function(event, context, callback) {
-        if (event["is-ping"]) context.succeed(true);
+        if (event["is-ping"]) {
+            context.succeed(true);
+            return;
+        }
         var invitedUserId = event.context.invitedUserId;
         var userId = event.context["authorizer-principal-id"];
         var pm = new Promise((resolve, reject) => {
 
         friendsFuncs.addFriend(userId, invitedUserId, new Date().toISOString(),
             function(result) {
-                context.succeed(result);
+                context.succeed(true);
             },
             function(error) {
                 callback(error);
