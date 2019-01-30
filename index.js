8f1be17860700fa6158e5d84203dd35fec0b5e53
CIPHER\d.edlow <d.edlow@cipher-sys.com>
2019-01-29 23:06:30 -0500

    File cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-var currentMod = require("functions/updateuser");
-
-// console.log(connection);
-exports.handler = async (event, context, callback) => {
-    await currentMod.handler(event, context, callback)
-};
