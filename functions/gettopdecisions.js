277e8488c53ce239fe0966b2e576831affbd6f72
Dan <dedlow10@gmail.com>
2019-03-02 10:58:00 -0500

    Converted perPage and offset to numbers.

--- functions/gettopdecisions.js
+++ functions/gettopdecisions.js
@@ -3,8 +3,8 @@ var decisionsFuncs = require("../decisions-funcs");
 module.exports = {
     handler: async function(event, context, callback) {
         var userId = event.context["authorizer-principal-id"];
-        var perPage = event.params.querystring.perPage || 20;
-        var offset = event.params.querystring.offset || 0;
+        var perPage = Number(event.params.querystring.perPage || 20);
+        var offset = Number(event.params.querystring.offset || 0);
         var days = event.params.querystring.days || 7;
         var pm = new Promise((resolve, reject) => {
             decisionsFuncs.getTopDecisions(userId, -(days), offset, perPage,
